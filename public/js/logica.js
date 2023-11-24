7a2e57aa8550f8b106c3a3b6b2ff5bb147744541
IKON3R <operezz2100@alumno.ipn.mx>
2023-11-23 23:11:58 -0600

    url2

--- public/js/logica.js
+++ public/js/logica.js
@@ -1,4 +1,4 @@
-var socket = io.connect('https://segundoint.onrender.com/');
+const socket = io.connect(segundoint.onrender.com);
 var list = document.querySelector('#lista-users');
 var username = window.location.pathname.replace('/chat/', '');
 var clientes = [];
